08762c3e8e52985d3b20021be47c6fb458887db6
Augusto Roman <augustoromanriver2005@gmial.com>
2026-04-16 20:42:28 +0200

    ejercicio 1 de clase 03

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,10 @@
+let nombre = 'Augusto';
+let edad = 21;
+let materia = 'desarrollo de software';
+
+const mensaje = `hola ${nombre}, tienes ${edad} años`;
+console.log(mensaje); 
+
+for (let i = 0; i < 3; i++) {
+    console.log(i); 
+}
